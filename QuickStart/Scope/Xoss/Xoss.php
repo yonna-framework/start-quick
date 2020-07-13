655fc9e75a6c919202dff5d7e86f32f143424355
hunzsig <mzyhaohaoren@qq.com>
2020-07-13 12:44:00 +0800

    up

--- QuickStart/Scope/Xoss/Xoss.php
+++ QuickStart/Scope/Xoss/Xoss.php
@@ -3,6 +3,7 @@
 namespace Yonna\QuickStart\Scope\Xoss;
 
 use Yonna\Database\DB;
+use Yonna\Database\Driver\Pdo\Where;
 use Yonna\Foundation\System;
 use Yonna\QuickStart\Helper\Assets;
 use Yonna\QuickStart\Scope\AbstractScope;
@@ -94,7 +95,6 @@ class Xoss extends AbstractScope
      */
     private function saveFile($fd)
     {
-        print_r($fd);
         $size = $fd['size'];
         if ($size > 0) {
             $data = file_get_contents($fd['tmp_name']);
@@ -121,82 +121,35 @@ class Xoss extends AbstractScope
         if (!System::dirCheck($saveData['path'], true)) {
             return $this->false('invalid dir');
         }
-        print_r($saveData);
         if (!is_file($saveData['uri'])) {
             $size = @file_put_contents($saveData['uri'], $data);
             if ($size === false) {
                 return $this->false('Save failed');
             }
         }
-        exit();
-
-
-        $dir = $hash[0];
-        $hash_file_name = $hashId . '.' . $fd['ext'];
-        $download_url = $hash[1] . $hash_file_name;
-        $filename = $fd['name'] ?: $hashId . '.' . $fd['ext'];
-        $dir = dirCheck($dir, true);
-        if (!$dir) return $this->false('dir not exist');
-        if (!is_file($dir . $hash_file_name)) {
-            $size = @file_put_contents($dir . $hash_file_name, $data);
-            if ($size === false) {
-                return $this->false('can not save data to the path');
-            }
+        if ($this->request()->getLoggingId()) {
+            $saveData['user_id'] = $this->request()->getLoggingId();
         }
-        $fileData = DB::connect()->table('assets')->equalTo('hash_id', $hashId)->one();
+        $fileData = DB::connect()->table('xoss')
+            ->field('key,name,suffix,size,content_type')
+            ->where(fn(Where $w) => $w->equalTo('hash', $hash))
+            ->one();
         if (!$fileData) {
-            $insertData = array(
-                'create_time' => $this->db()->now(),
-                'uid' => array($this->getBean()->getAuthUid()),
-                'hash_id' => $hashId,
-                'hash_file_name' => $hash_file_name,
-                'file_name' => $filename,
-                'file_ext' => $fd['ext'],
-                'file_size' => $size,
-                'content_type' => $fd['type'],
-                'from_url' => $fd['tmp_name'],
-                'download_url' => $download_url,
-                'path' => $dir . $hash_file_name,
-            );
-            try {
-                $this->db()->table('assets')->insert($insertData);
-            } catch (\Exception $e) {
-                return $this->false($e->getMessage());
-            }
-            $fileData = $this->db()->table('assets')->equalTo('hash_id', $hashId)->one();
+            DB::connect()->table('xoss')->insert($saveData);
+            $fileData = DB::connect()->table('xoss')
+                ->field('key,name,suffix,size,content_type')
+                ->where(fn(Where $w) => $w->equalTo('hash', $hash))
+                ->one();
             if (!$fileData) {
                 return $this->false('file data error');
             }
         }
-        if (!in_array($this->getBean()->getAuthUid(), $fileData['assets_uid'])) {
-            $fileData['assets_uid'][] = $this->getBean()->getAuthUid();
-            $updateData = array(
-                'uid' => $fileData['assets_uid'],
-                'update_time' => $this->db()->now(),
-            );
-            try {
-                $this->db()->table('assets')->equalTo('hash_id', $hashId)->update($updateData);
-            } catch (\Exception $e) {
-            }
-        }
-        $fileData['assets_error'] = 0;
-        $fileData['assets_file_image'] = $this->getHost() . $this->assetsHelper()->getImageByContentType($fileData['assets_file_ext'], $fileData['assets_download_url']);
-        $fileData['assets_download_url'] = $this->getHost() . $download_url;
-        unset($fileData['assets_uid']);
-        unset($fileData['assets_hash_id']);
-        unset($fileData['assets_path']);
-        unset($fileData['assets_from_url']);
-        unset($fileData['assets_call_qty']);
-        unset($fileData['assets_call_last_time']);
-        unset($fileData['assets_create_time']);
-        unset($fileData['assets_update_time']);
         return $fileData;
     }
 
     private function analysisFile()
     {
         $files = $this->request()->getFiles();
-        print_r($files);
         $results = [];
         foreach ($files as $fd) {
             $fd = $this->checkFileData($fd);
@@ -204,18 +157,22 @@ class Xoss extends AbstractScope
                 $results[] = [
                     "result" => 0,
                     "msg" => $this->falseMsg,
+                    "data" => null,
                 ];
             } else {
-                $this->saveFile($fd);
+                $results[] = [
+                    "result" => 1,
+                    "msg" => 'success',
+                    "data" => $this->saveFile($fd)
+                ];
             }
         }
-
+        return $results;
     }
 
     public function upload()
     {
-        $this->analysisFile();
-        return true;
+        return $this->analysisFile();
     }
 
 }
