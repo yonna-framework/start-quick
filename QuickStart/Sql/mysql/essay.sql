d22e041e9bdb57d5a561714d3d882badff406d87
hunzsig <mzyhaohaoren@qq.com>
2020-07-16 22:47:47 +0800

    up

--- QuickStart/Sql/mysql/essay.sql
+++ QuickStart/Sql/mysql/essay.sql
@@ -1,13 +1,11 @@
 create table `essay_category`
 (
-    `id`          bigint unsigned auto_increment not null comment 'id',
-    `upper_id`    bigint unsigned                not null default 0 comment 'essay_category_id',
-    `name`        char(255)                      not null default '' comment '分类名称',
-    `level`       bigint unsigned                not null default 1 comment '分类等级',
-    `description` varchar(1024)                  not null default '' comment '描述',
-    `logo`        json comment '分类logo图',
-    `status`      tinyint                        not null default 1 comment '状态[-1审核驳回,1待审核,2审核通过]',
-    `ordering`    int                            not null default 0 comment '排序[降序]',
+    `id`       bigint unsigned auto_increment not null comment 'id',
+    `upper_id` bigint unsigned                not null default 0 comment 'essay_category_id',
+    `name`     char(255)                      not null default '' comment '分类名称',
+    `level`    bigint unsigned                not null default 1 comment '分类等级',
+    `status`   tinyint                        not null default 1 comment '状态[-1审核驳回,1待审核,2审核通过]',
+    `ordering` int                            not null default 0 comment '排序[降序]',
     primary key (`id`),
     unique key (`upper_id`, `name`),
     index (`level`),
@@ -16,12 +14,10 @@ create table `essay_category`
 
 create table `essay_type`
 (
-    `id`          bigint unsigned auto_increment not null comment 'id',
-    `name`        char(255)                      not null default '' comment '类型名称',
-    `description` varchar(1024)                  not null default '' comment '描述',
-    `logo`        json comment '分类logo图',
-    `status`      tinyint                        not null default 1 comment '状态[-1审核驳回,1待审核,2审核通过]',
-    `ordering`    int                            not null default 0 comment '排序[降序]',
+    `id`       bigint unsigned auto_increment not null comment 'id',
+    `name`     char(255)                      not null default '' comment '类型名称',
+    `status`   tinyint                        not null default 1 comment '状态[-1审核驳回,1待审核,2审核通过]',
+    `ordering` int                            not null default 0 comment '排序[降序]',
     primary key (`id`),
     unique key (`name`),
     index (`status`)
@@ -34,8 +30,9 @@ create table `essay`
     `type_id`     bigint unsigned                not null default 0 comment 'essay_type_id',
     `category_id` bigint unsigned                not null default 0 comment 'essay_category_id',
     `status`      tinyint                        not null default 1 comment '状态[-1无效,1有效]',
+    `likes`       bigint unsigned                not null default 0 comment '点赞量',
     `views`       bigint unsigned                not null default 0 comment '浏览量',
-    `data`        json comment '文章数据',
+    `content`     text                           not null default '' comment '文章内容',
     `ordering`    int                            not null default 0 comment '排序[降序]',
     primary key (`id`),
     index (`user_id`, `type_id`, `category_id`, `status`)
---
+++ QuickStart/Sql/mysql/league.sql
@@ -0,0 +1,78 @@
+CREATE TABLE `data_hobby`
+(
+    `id`   bigint unsigned auto_increment not null comment 'id',
+    `name` char(255)                      not null comment '名称'
+) ENGINE = INNODB COMMENT '兴趣爱好';
+
+CREATE TABLE `data_work`
+(
+    `id`   bigint unsigned auto_increment not null comment 'id',
+    `name` char(255)                      not null comment '名称'
+) ENGINE = INNODB COMMENT '职业工作';
+
+CREATE TABLE `data_speciality`
+(
+    `id`   bigint unsigned auto_increment not null comment 'id',
+    `name` char(255)                      not null comment '名称'
+) ENGINE = INNODB COMMENT '特长';
+
+CREATE TABLE `league`
+(
+    `id`                   bigint unsigned auto_increment not null comment 'id',
+    `master_user_id`       bigint unsigned                not null comment '社团拥有者user_id',
+    `name`                 char(255)                      not null comment '社团名',
+    `business_license_pic` char(255)                      not null comment '营业执照',
+    `status`               tinyint                        not null default 1 comment '状态[-2作废,-1审核驳回,1待审核,2审核通过]',
+    `apply_reason`         char(255)                      not null default '' comment '申请理由',
+    `rejection_reason`     char(255)                      not null default '' comment '驳回理由',
+    `passed_reason`        char(255)                      not null default '' comment '通过理由',
+    `delete_reason`        char(255)                      not null default '' comment '作废理由',
+    `apply_datetime`       datetime                                default '1970-01-01 00:00:00' not null comment '申请日期时间',
+    `rejection_datetime`   datetime                                default '1970-01-01 00:00:00' not null comment '驳回日期时间',
+    `pass_datetime`        datetime                                default '1970-01-01 00:00:00' not null comment '通过日期时间',
+    `delete_datetime`      datetime                                default '1970-01-01 00:00:00' not null comment '作废日期时间',
+    PRIMARY KEY (`id`),
+    INDEX (`master_user_id`),
+    INDEX (`name`)
+) ENGINE = INNODB COMMENT '社团';
+
+CREATE TABLE `league_associate_administrator`
+(
+    `league_id`          bigint unsigned not null comment '社团id',
+    `user_id`            bigint unsigned not null comment '管理员user_id',
+    `status`             tinyint         not null default 1 comment '状态[-2作废,-1审核驳回,1待审核,2审核通过]',
+    `apply_reason`       char(255)       not null default '' comment '申请理由',
+    `rejection_reason`   char(255)       not null default '' comment '驳回理由',
+    `passed_reason`      char(255)       not null default '' comment '通过理由',
+    `delete_reason`      char(255)       not null default '' comment '作废理由',
+    `apply_datetime`     datetime                 default '1970-01-01 00:00:00' not null comment '申请日期时间',
+    `rejection_datetime` datetime                 default '1970-01-01 00:00:00' not null comment '驳回日期时间',
+    `pass_datetime`      datetime                 default '1970-01-01 00:00:00' not null comment '通过日期时间',
+    `delete_datetime`    datetime                 default '1970-01-01 00:00:00' not null comment '作废日期时间',
+    INDEX (`league_id`),
+    INDEX (`user_id`)
+) ENGINE = INNODB COMMENT '社团与管理员用户关系';
+
+CREATE TABLE `league_associate_hobby`
+(
+    `league_id` bigint unsigned not null comment '社团id',
+    `data_id`   bigint unsigned not null comment '爱好id',
+    INDEX (`league_id`),
+    INDEX (`data_id`)
+) ENGINE = INNODB COMMENT '社团与爱好关系';
+
+CREATE TABLE `league_associate_work`
+(
+    `league_id` bigint unsigned not null comment '社团id',
+    `data_id`   bigint unsigned not null comment '职业工作id',
+    INDEX (`league_id`),
+    INDEX (`data_id`)
+) ENGINE = INNODB COMMENT '社团与职业工作关系';
+
+CREATE TABLE `league_associate_speciality`
+(
+    `league_id` bigint unsigned not null comment '社团id',
+    `data_id`   bigint unsigned not null comment '特长id',
+    INDEX (`league_id`),
+    INDEX (`data_id`)
+) ENGINE = INNODB COMMENT '社团与特长关系';
